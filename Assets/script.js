1de27721c93c99dc623df503cc6ca63c37458d2b
daisyisibor <daisyisibor8@gmail.com>
2024-02-25 15:06:23 -0500

    creating a function to display the temp, wind speed, humidity

--- Assets/script.js
+++ Assets/script.js
@@ -8,42 +8,86 @@ var searchBtn=document.getElementById("searchBtn");
 var apiKey = '41428fb0a9890c868f611f78c6401a98'
 
 
-// create a function for the api call and fetch weather data for inputed city, note : url has weather 
-async function apiCall(city){
+// create a function for the current weather datat call and fetch weather data for inputed city, note : url has weather 
+ function getweatherCall(city){
   
     var apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
 
-    await fetch(apiUrl)
+  return fetch(apiUrl)
     .then(function(response){
         return response.json();
     })
     .then(function(data){
-        console.log(data)
+    console.log(data);
+    return data;
 
     })
+    .catch(function(error) {
+        console.error('Error fetching current weather data:', error);
+        throw error;
+    });
 }
 
 
 // function to fetch the data for given city note: url has forcast
 
-async function getWeather(city){
+function getForecast(city){
     var apiUrl =`http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
 
-    await fetch(apiUrl)
+    return fetch(apiUrl)
     .then(function(response){
         return response.json();
     })
     .then(function(data){
         console.log(data)
+        return data;
 
     })
-
+    .catch(function(error) {
+        console.error('Error fetching forecast data:', error);
+        throw error;
+    });
 
 }
 
 
-/
+//function to display the eather forecast information when city is inputed 
+
+ function showWeather(){
+    var city = document.getElementById("cityName").value;
+
+   getweatherCall(city)
+   .then(function(weatherCallData){
+    var todayWeatherInfo= "Current Weather:<br>" +
+    "Temperature: " + weatherCallData.main.temp + 'K<br>' + 
+    "Weather Condition: " + weatherCallData.weather[0]. description + '<br>' +
+    "Wind Speed: "  + weatherCallData.wind.speed + 'm/s<br>' + 
+    "Humidity: " + weatherCallData.main.humidity + '%';
+
+    document.getElementById("todayWeather").innerHtml=todayWeatherInfo;
+    return getForecast(city);
+
+})
+
+ }
 
 
 
-searchBtn.addEventListener('click', apiCall);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ document.addEventListener('DOMContentLoaded', function() {
+    var button = document.getElementById('searchBtn');
+    button.addEventListener('click', showWeather);
+});
