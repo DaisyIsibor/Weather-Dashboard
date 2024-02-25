213bb95ddf5e50578858996f09c7bd1624fb1bde
daisyisibor <daisyisibor8@gmail.com>
2024-02-25 13:50:28 -0500

    fetch request for the forecast

--- Assets/script.js
+++ Assets/script.js
@@ -3,24 +3,47 @@ var citySearch = document.getElementById("city-Search");
 var  form = document.getElementById("searchForm");
 var searchBtn=document.getElementById("searchBtn");
 
-// create a function for the api call 
-function apiCall(){
+
+//Api Key
+var apiKey = '41428fb0a9890c868f611f78c6401a98'
+
+
+// create a function for the api call and fetch weather data for inputed city, note : url has weather 
+async function apiCall(city){
   
-    var apiKey = '41428fb0a9890c868f611f78c6401a98'
     var apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
 
-    fetch(apiUrl)
+    await fetch(apiUrl)
     .then(function(response){
         return response.json();
     })
     .then(function(data){
         console.log(data)
 
-        for (var i = 0; i < data.length; i++){
-            var displayContent= document.getElementById("display");
-        }
     })
 }
+
+
+// function to fetch the data for given city note: url has forcast
+
+async function getWeather(city){
+    var apiUrl =`http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
+
+    await fetch(apiUrl)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data)
+
+    })
+
+
+}
+
+
+/
+
 
 
 searchBtn.addEventListener('click', apiCall);
