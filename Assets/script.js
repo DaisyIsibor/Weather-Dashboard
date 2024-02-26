bda4db9c5fa26fb7176736963bb0346aed361eb2
daisyisibor <daisyisibor8@gmail.com>
2024-02-25 21:23:38 -0500

    changed js code, addec metric to the url instead of converting kelvin to celsius

--- Assets/script.js
+++ Assets/script.js
@@ -8,119 +8,113 @@ var citySearch = document.getElementById("city-Search");
 var apiKey = '41428fb0a9890c868f611f78c6401a98'
 
 
-// create a function for the current weather datat call and fetch weather data for inputed city, note : url has weather 
- function getweatherCall(city){
-  
-    var apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
-
-  return fetch(apiUrl)
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(data){
-    console.log(data);
-    return data;
-
-    })
-    .catch(function(error) {
-        console.error('Error fetching current weather data:', error);
-        throw error;
-    });
-}
-
-
-// function to fetch the data for given city note: url has forcast
-
-function getForecast(city){
-    var apiUrl =`http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
+// Function to fetch current weather data for a given city added metric to URL
+function getweatherCall(city) {
+    var apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
     return fetch(apiUrl)
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(data){
-        console.log(data)
-        return data;
-
-    })
-    .catch(function(error) {
-        console.error('Error fetching forecast data:', error);
-        throw error;
-    });
-
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data);
+            return data;
+        })
+        .catch(function(error) {
+            console.error('Error fetching current weather data:', error);
+            throw error;
+        });
 }
 
+// Function to fetch the 5-day forecast data for a given city
+function getForecast(city) {
+    var apiUrl = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
 
-//function to display the eather forecast information when city is inputed 
+    return fetch(apiUrl)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data);
+            return data;
+        })
+        .catch(function(error) {
+            console.error('Error fetching forecast data:', error);
+            throw error;
+        });
+}
 
- function showWeather(){
+// Function to display the weather forecast
+function showWeather() {
     var city = document.getElementById("cityName").value;
-
-    // this is a function to display the forecast weather for today and this include the name of the city, date, time and other information  to display 
+   // this displays the Name of the city, dates and this is displayed per local time and added some styles
     getweatherCall(city)
-        .then(function(weatherCallData) {
-            var cityName = "<strong style='font-size: larger;'>" + weatherCallData.name + "</strong>";
-            var dateTime = new Date();
-            var date = "<strong style='font-size: larger;'>" + dateTime.toLocaleDateString() + "</strong>";
-           
-            // the br tag is just to display each result stack on each other 
-
-             // (weatherCallData.main.temp  - 273.15) calculates the temperature in Celsius, and .toFixed(2) rounds it to two decimal places.  0 Kelvin (-273.15 degrees Celsius) is absolute zero,  convert Kelvin to Celsius, you subtract 273.15 from the temperature in Kelvin
-
+    .then(function(weatherCallData) {
+        var cityName = "<strong style='font-size: larger;'>" + weatherCallData.name + "</strong>";
+        var dateTime = new Date();
+        var date = "<strong style='font-size: larger;'>" + dateTime.toLocaleDateString() + "</strong>";
+          // this just displays the time in parentheses and in a format
             var formattedDateTime = cityName + " (" + date + ")";
-            var temperatureCelsius = (weatherCallData.main.temp - 273.15).toFixed(2); // Convert temperature to Celsius
+
+            var weatherIconUrl = "http://openweathermap.org/img/wn/" + weatherCallData.weather[0].icon + ".png";
+            var temperatureCelsius = weatherCallData.main.temp.toFixed(2); // Temperature is already in Celsius by adding &unit=metric on url
+
+            // this displayed todays's wather or current weather and weather icon 
 
 
-           var todayWeatherInfo = "Current Weather for " + formattedDateTime + ":<br>" +// this displays city, date and time in one role 
 
-             "<img src='http://openweathermap.org/img/wn/" + weatherCallData.weather[0].icon + ".png'><br>" +
-            // this displays the image instead of the description, the link on the code directs you to the images on open weather API.
+            var currentWeatherInfo = "Current Weather for " + formattedDateTime + ":<br>" + "<img src='" + weatherIconUrl + "' alt='Weather Icon'><br>" +
+            "Temperature: " + temperatureCelsius + '&deg;C<br>' +
+            "Wind: " + weatherCallData.wind.speed + 'm/s<br>' +
+            "Humidity: " + weatherCallData.main.humidity + '%<br>';
 
-            "Temp: " + temperatureCelsius + '&deg;C<br>' + // Display temperature in Celsius
-
-                "Wind: " + weatherCallData.wind.speed + 'm/s<br>' + // this displays the wind speed in meters per seconds
-
-                "Humidity: " + weatherCallData.main.humidity + '%'; // this displays the humidity 
-
-            document.getElementById("todayWeather").innerHTML = todayWeatherInfo;
+            document.getElementById("todayWeather").innerHTML = currentWeatherInfo;
             return getForecast(city);
         })
+        // this is to display 5 day forcast since current day already displayed 
         .then(function(forecastData) {
             var forecastInfo = '5-Day Forecast:<br>';
             var currentDate = new Date();
-            currentDate.setDate(currentDate.getDate() + 1); // Set the date to tomorrow
-        
-            for (var i = 0; i < 4; i++) {
-                var nextDayForecast = forecastData.list[i]; // Get the forecast data for the next day
-        
+            currentDate.setDate(currentDate.getDate() + 1); // Set the date to tomorrow 
+
+
+
+            // a loop to add one more date unit it's up to 5 days 
+            for (var i = 0; i < 5; i++) {
+                var nextDayForecast = forecastData.list[i]; // Get the forecast data for the next day after current date is displayed 
+
+
+                // condition for next day displays 
                 if (nextDayForecast) {
                     var date = currentDate.toLocaleDateString();
+
+                    // This is declared because it is displayed , this is to display the images description of weather img ,link will take you to the imges link on open weather API
                     var weatherIcon = "<img src='http://openweathermap.org/img/wn/" + nextDayForecast.weather[0].icon + ".png'>";
                     var weatherDescription = nextDayForecast.weather[0].description;
+                    var temperatureCelsius = nextDayForecast.main.temp.toFixed(2); // Temperature is already in Celsius
 
-                // (dayForecast.main.temp - 273.15) calculates the temperature in Celsius, and .toFixed(2) rounds it to two decimal places.  0 Kelvin (-273.15 degrees Celsius) is absolute zero,  convert Kelvin to Celsius, you subtract 273.15 from the temperature in Kelvin
-                var temperatureCelsius = (nextDayForecast.main.temp - 273.15).toFixed(2);
-            var humidity = nextDayForecast.main.humidity;
+                    // displays the humidity
+                    var humidity = nextDayForecast.main.humidity;
 
-            forecastInfo += "<strong>" + date + "</strong>:<br>" +
-                "Weather Condition: " + weatherIcon + " " + weatherDescription + '<br>' +
-                "Temperature: " + temperatureCelsius + "&deg;C<br>" +
-                "Humidity: " + humidity + "%<br><br>";
-        }
+                    //this displays in below format when shown
+                    forecastInfo += "<strong>" + date + "</strong>:<br>" +
+                        "Weather Condition: " + weatherIcon + " " + weatherDescription + '<br>' +
+                        "Temperature: " + temperatureCelsius + "&deg;C<br>" +
+                        "Humidity: " + humidity + "%<br><br>";
+                }
 
-        currentDate.setDate(currentDate.getDate() + 1); // Move to the next day
-    }
+              //  currentDate.setDate(currentDate.getDate() + 1); // Move to the next day
+            }
 
-    document.getElementById("displays").innerHTML = forecastInfo;
-})
-.catch(function(error) {
-    console.error('Error fetching forecast data:', error);
-    document.getElementById("displays").innerHTML = "Error fetching forecast data. Please try again later.";
-})
+            document.getElementById("displays").innerHTML = forecastInfo;
+        })
+        .catch(function(error) {
+            console.error('Error in showWeather:', error);
+        });
+}
 
- };
-
+// Event listener to call showWeather function on button click
 document.addEventListener('DOMContentLoaded', function() {
-var button = document.getElementById('searchBtn');
-button.addEventListener('click', showWeather);
-})
+    var button = document.getElementById('searchBtn'); 
+    button.addEventListener('click', showWeather);
+});
